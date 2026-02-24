a6583a6a60a513af0831daa4652107f9fcdfb4ce
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2026-02-24 13:33:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// =====================
+// DADOS
+// =====================
+var cartas = [
+  { 
+    nome: "Raposa",
+    imagem: "1.jpeg", 
+    atributos: 
+    { 
+      ataque: 82, 
+      defesa: 75, 
+      magia: 90 
+    } 
+  } ,
+  { 
+    nome: "Foxie", 
+    imagem: "2.jpeg", 
+    atributos: 
+    { 
+      ataque: 80, 
+      defesa: 70, 
+      magia: 60 
+    } 
+  },
+  { 
+    nome: "Chicken", 
+    imagem: "3.jpeg", 
+    atributos: { 
+      ataque: 85, 
+      defesa: 72, 
+      magia: 58 
+    } 
+  }
+];
+
+var cartaJogador = null;
+var cartaMaquina = null;
+
+var URL_MOLDURA = "https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png";
+
+// =====================
+// UTILITÁRIOS
+// =====================
+function aleatorio(max) {
+  
+}
+
+function sorteiaCarta() {
+ 
+}
+
+function sorteiaCartaDiferente(cartaProibida) {
+ 
+}
+
+
+
+// =====================
+// UI (TELA)
+// =====================
+function desenhaCarta(divId, carta, modo) {
+  // modo = "jogador" (com radio) ou "maquina" (texto)
+  
+}
+
+function setBotoes(jaSorteou) {
+ 
+}
+
+
+// =====================
+// AÇÕES
+// =====================
+function sortearCarta() {
+  
+}
+
